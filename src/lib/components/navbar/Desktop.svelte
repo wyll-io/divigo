f0a29bc0fd4e66255c2c7dd7632c1d70206e8d3a
DataHearth <dev@antoine-langlois.net>
2024-07-03 11:19:15 +0200

    update navbar

--- src/lib/components/navbar/Desktop.svelte
+++ src/lib/components/navbar/Desktop.svelte
@@ -2,15 +2,15 @@
   import { cn } from '$lib/utils';
   import type { ComponentType } from 'svelte';
   import { page } from '$app/stores';
-  import { Settings } from 'lucide-svelte';
+  import { CircleUser, LogIn, LogOut } from 'lucide-svelte';
 
   export let links: Array<{ label: string; icon: ComponentType; link: string }>;
 
   const currentPage = $page.url.pathname;
 </script>
 
-<div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-  <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4">
+<div class="hidden lg:inset-y-0 lg:z-50 lg:flex lg:h-full lg:w-72 lg:flex-col">
+  <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-blue-600 px-6 pb-4">
     <div class="flex h-16 shrink-0 items-center">
       <img
         class="h-8 w-auto"
@@ -46,24 +46,66 @@
           </ul>
         </li>
 
-        <li class="mt-auto">
-          <a
-            href="/settings"
-            class={cn(
-              'nav-btn group',
-              currentPage === '/settings' ? 'nav-btn-active' : 'nav-btn-default'
-            )}
-          >
-            <Settings
+        <div class="mt-auto space-y-1">
+          <div class="relative mb-3">
+            <div class="absolute inset-0 flex items-center" aria-hidden="true">
+              <div class="w-full border-t border-gray-50 opacity-40"></div>
+            </div>
+          </div>
+
+          <div>
+            <a
+              href="/profile"
               class={cn(
-                'nav-btn-icon group',
-                currentPage === '/settings' ? 'nav-btn-icon-active' : 'nav-btn-icon-default'
+                'nav-btn group items-center',
+                currentPage === '/profile' ? 'nav-btn-active' : 'nav-btn-default'
               )}
-              aria-hidden="true"
-            />
-            Settings
-          </a>
-        </li>
+            >
+              {#if false}
+                <img
+                  class="h-8 w-8 rounded-full bg-blue-700"
+                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt="Profile"
+                />
+              {:else}
+                <CircleUser
+                  class={cn(
+                    'nav-btn-icon',
+                    currentPage === '/profile' ? 'nav-btn-icon-active' : 'nav-btn-icon-default'
+                  )}
+                  aria-hidden="true"
+                />
+              {/if}
+              <span class="sr-only">Your profile</span>
+              <span aria-hidden="true">{false ? 'Tim Cook' : 'Profile'}</span>
+            </a>
+          </div>
+          <div>
+            {#if false}
+              <a
+                href="/logout"
+                class={cn(
+                  'nav-btn group',
+                  currentPage === '/settings' ? 'nav-btn-active' : 'nav-btn-default'
+                )}
+              >
+                <LogOut class="nav-btn-icon nav-btn-icon-default" aria-hidden="true" />
+                Logout
+              </a>
+            {:else}
+              <a
+                href="/login"
+                class={cn(
+                  'nav-btn group',
+                  currentPage === '/settings' ? 'nav-btn-active' : 'nav-btn-default'
+                )}
+              >
+                <LogIn class="nav-btn-icon nav-btn-icon-default" aria-hidden="true" />
+                Login
+              </a>
+            {/if}
+          </div>
+        </div>
       </ul>
     </nav>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,6 @@ import forms from '@tailwindcss/forms';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {}
-  },
+  theme: {},
   plugins: [forms]
 };
